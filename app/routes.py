636805ac71cddab73135527e807c90071af18955
syedzubeen <zubeenqadry@gmail.com>
2023-08-12 21:53:21 +0530

    cleanup and added a .gitignore

--- app/routes.py
+++ app/routes.py
@@ -14,19 +14,14 @@ async def fetch_recipe():
 
     query = "SELECT answer FROM mindsdb.recipemaster6 WHERE question = 'Start your response with the sentence Hola Amigo !  Do not add salutations or anything else to your response. Behave like a chef who knows a ton of different recipes. You are responding to an audience that is looking to loose weight. Now suggest me just one new recipe each time I ask you, in the form of bullet points, that is healthy and easy to cook?';"
     resp = session.post('https://cloud.mindsdb.com/api/sql/query', json={'query': query})
-
-    #query_calories = "answer in exactly this format: It has xx calories. This is the question: What are the number of calories in an average sized turnip"
-    #resp_calories = session.post('https://cloud.mindsdb.com/api/sql/query', json={'query': query})
     
     json_response = resp.json()
-    #json_response2 = resp_calories.json()
+
     # Assuming there's only one element in the inner list
     data_value = json_response['data'][0][0]  
-    #data_value2 = json_response['data'][0][0] 
     
     # Remove special characters from start and end
     data_value = data_value.strip('[\n]').strip()
-    #data_value2 = data_value2.strip('[\n]').strip()
 
     #Debug and print value
     #print(data_value2)
@@ -53,10 +48,10 @@ async def fetch_calories():
     #if 'data' in json_response_calories and json_response_calories['data']:
     data_value_calories = json_response_calories['data'][0][0]
 
-        # Remove special characters from start and end
+    # Remove special characters from start and end
     data_value_calories = data_value_calories.strip('[\n]').strip()
 
-        # Debug and print value
+    # Debug and print value
     print(data_value_calories)
 
     if resp_calories.status_code == 200:
