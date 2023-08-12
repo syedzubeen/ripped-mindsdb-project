62a5aa7330259ba617df831fe68fa43a1b203972
syedzubeen <zubeenqadry@gmail.com>
2023-08-12 22:30:31 +0530

    update for picking the correct env vars

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,15 @@
-import asyncio
+import os
 from flask import render_template, request, json, jsonify
-import requests
+import requests, asyncio
 
 from app import app
 
 
-async def fetch_recipe():
+async def fetch_recipe(username, password):
     session = requests.Session()
     session.post('https://cloud.mindsdb.com/cloud/login', json={
-        'email': 'zubeenqadry@gmail.com',
-        'password': 'Asdf@1234'
+        'email': username,
+        'password': password
     })
 
     query = "SELECT answer FROM mindsdb.recipemaster6 WHERE question = 'Start your response with the sentence Hola Amigo !  Do not add salutations or anything else to your response. Behave like a chef who knows a ton of different recipes. You are responding to an audience that is looking to loose weight. Now suggest me just one new recipe each time I ask you, in the form of bullet points, that is healthy and easy to cook?';"
@@ -31,11 +31,11 @@ async def fetch_recipe():
     else:
         return 'Error fetching recipe'
     
-async def fetch_calories():
+async def fetch_calories(username, password):
     session = requests.Session()
     session.post('https://cloud.mindsdb.com/cloud/login', json={
-        'email': 'zubeenqadry@gmail.com',
-        'password': 'Asdf@1234'
+        'email': username,
+        'password': password
     })
     food_item_to_check = request.form['food_item']
     custom_query = "SELECT answer FROM mindsdb.recipemaster6 WHERE question = 'answer in exactly this format: It has xx calories. This is the question: What are the number of calories in an average sized" + food_item_to_check +"?';"
@@ -60,17 +60,43 @@ async def fetch_calories():
         return 'Error fetching calories'
 
 
+def read_env_vars():
+    current_file_directory = os.path.dirname(os.path.abspath(__file__))
+    env_file_path = os.path.join(current_file_directory, 'env_vars.txt')
+
+    username = None
+    password = None
+
+    with open(env_file_path) as file:
+        for line in file:
+            key, value = line.strip().split('=')
+            if key == 'USERNAME':
+                username = value
+            elif key == 'PASSWORD':
+                password = value
+
+    return username, password
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+
+
     if request.method == 'POST':
+
+        username, password = read_env_vars()
+
+        if username is None or password is None:
+            return "Credentials not found."
+
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
 
         # Fetch recipe asynchronously
-        recipe_response = loop.run_until_complete(fetch_recipe())
+        recipe_response = loop.run_until_complete(fetch_recipe(username, password))
 
         # Fetch calories asynchronously
-        calories_response = loop.run_until_complete(fetch_calories())
+        calories_response = loop.run_until_complete(fetch_calories(username, password))
 
         loop.close()
 
